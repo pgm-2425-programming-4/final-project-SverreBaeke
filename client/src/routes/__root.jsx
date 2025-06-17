23b6315c8a35ded33fdc7ac4e6badf966e8a46d9
SverreBaeke <sverre.baeke@hotmail.com>
2025-06-17 14:25:01 +0200

    feat: enhance routing with error handling and project fetching logic

--- client/src/routes/__root.jsx
+++ client/src/routes/__root.jsx
@@ -1,12 +1,34 @@
-import { createRootRoute } from "@tanstack/react-router";
+import { createRootRoute, notFound } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import AppLayout from "../components/AppLayout/AppLayout";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { AppLayout } from "../components/AppLayout/AppLayout";
+import { fetchProjects } from "../data/fetchProjects.js";
+
+const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
+  component: RouteComponent,
+  loader: async () => {
+    const projects = await fetchProjects();
+    if (!projects) {
+      throw notFound({ data: "NO_PROJECTS" });
+    }
+    return projects;
+  },
+
+  notFoundComponent: ({ data }) => {
+    if (data.data === "NO_PROJECTS") {
+      return <p>No projects found!</p>;
+    }
+    return <p>Something went wrong!</p>;
+  },
+});
+
+function RouteComponent() {
+  return (
+    <QueryClientProvider client={queryClient}>
       <AppLayout />
       <TanStackRouterDevtools />
-    </>
-  ),
-});
+    </QueryClientProvider>
+  );
+}

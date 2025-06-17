efde656d2eecdf142bde491dab52b22b5c6bff8e
SverreBaeke <sverre.baeke@hotmail.com>
2025-06-17 14:27:27 +0200

    feat: implement project loading by projectId in route component

--- client/src/routes/projects/$projectId/index.jsx
+++ client/src/routes/projects/$projectId/index.jsx
@@ -1,9 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { fetchProjectsById } from "../../../data/fetchProjects";
 
 export const Route = createFileRoute("/projects/$projectId/")({
+  loader: async ({ params }) => {
+    const projectId = params.projectId;
+    const data = await fetchProjectsById(projectId);
+    
+    return data
+  },
   component: RouteComponent,
+  
 });
 
 function RouteComponent() {
-  return <div>Hello "/projects/"!</div>;
+  const projects = Route.useLoaderData();
+  console.log(projects)
+  return <div>{projects.name}</div>;
 }

dd0d993d9c2bb18544b62e85cbb5c166e5f7b418
SverreBaeke <sverre.baeke@hotmail.com>
2025-06-17 14:26:01 +0200

    feat: enhance Sidebar component to load and display projects dynamically

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,40 +1,51 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useLoaderData } from "@tanstack/react-router";
 import "./Sidebar.css";
 
-export default function Sidebar() {
+export function Sidebar() {
+  const projects = useLoaderData({ from: "__root__" });
+
   return (
-    <nav className="sidebar">
-      <section className="sidebar__section">
-        <ul className="sidebar__list">
-          <li className="sidebar__item">
-            <Link to="/" className="sidebar__link">
-              Home
-            </Link>
-          </li>
-        </ul>
-      </section>
+    <aside className="app-layout__sidebar">
+      <nav className="sidebar">
+        <section className="sidebar__section">
+          <ul className="sidebar__list">
+            <li className="sidebar__item">
+              <Link to="/" className="sidebar__link">
+                Home
+              </Link>
+            </li>
+          </ul>
+        </section>
 
-      <section className="sidebar__section">
-        <div className="sidebar__subtitle">Projects</div>
-        <ul className="sidebar__list">
-          <li className="sidebar__item">
-            <Link to="/projects/$projectId" className="sidebar__link">
-              Projecten
-            </Link>
-          </li>
-        </ul>
-      </section>
+        <section className="sidebar__section">
+          <div className="sidebar__subtitle">Projects</div>
+          <ul className="sidebar__list">
+            {projects.map((project) => {
+              return (
+                <li className="sidebar__item" key={project.documentId}>
+                  <Link
+                    to={`/projects/${project.documentId}`}
+                    className="sidebar__link"
+                  >
+                    {project.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
 
-      <section className="sidebar__section">
-        <div className="sidebar__subtitle">Info</div>
-        <ul className="sidebar__list">
-          <li className="sidebar__item">
-            <Link to="/about" className="sidebar__link">
-              About
-            </Link>
-          </li>
-        </ul>
-      </section>
-    </nav>
+        <section className="sidebar__section">
+          <div className="sidebar__subtitle">Info</div>
+          <ul className="sidebar__list">
+            <li className="sidebar__item">
+              <Link to="/about" className="sidebar__link">
+                About
+              </Link>
+            </li>
+          </ul>
+        </section>
+      </nav>
+    </aside>
   );
 }

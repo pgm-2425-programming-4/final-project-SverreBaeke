000e95fc2bc93560c75bf70817e2f536a5b12a1e
SverreBaeke <sverre.baeke@hotmail.com>
2025-06-17 22:19:29 +0200

    feat: add TaskModal and task selection handling in project route

--- client/src/routes/projects/$projectId/index.jsx
+++ client/src/routes/projects/$projectId/index.jsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { fetchProjectsById } from "../../../data/fetchProjects";
 import { TaskCard } from "../../../components/TaskCard/TaskCard";
+import { TaskModal } from "../../../components/TaskModal/TaskModal";
+import { useState } from "react";
 
 export const Route = createFileRoute("/projects/$projectId/")({
   loader: async ({ params }) => {
@@ -15,15 +17,35 @@ export const Route = createFileRoute("/projects/$projectId/")({
 function RouteComponent() {
   const project = Route.useLoaderData();
   const tasks = project.tasks;
-  console.log(tasks[0].state)
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function handleTaskClick(task) {
+    setSelectedTask(task);
+    setIsModalOpen(true);
+  }
+
+  function handleCloseModal() {
+    setIsModalOpen(false);
+    setSelectedTask(null);
+  }
 
   return (
     <>
-    {tasks.map((task) => {
-       return <TaskCard task={task}/>
-    })}
-      
-
+      {tasks.map((task) => {
+        return (
+          <TaskCard
+            key={task.id}
+            task={task}
+            onClick={() => handleTaskClick(task)}
+          />
+        );
+      })}
+      <TaskModal
+        task={selectedTask}
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+      />
     </>
   );
 }

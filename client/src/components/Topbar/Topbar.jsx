330e0e4423792f491f27d22e72713d13a95a0d5b
SverreBaeke <sverre.baeke@hotmail.com>
2025-06-17 14:27:46 +0200

    feat: update Topbar component to utilize projectId for dynamic routing

--- client/src/components/Topbar/Topbar.jsx
+++ client/src/components/Topbar/Topbar.jsx
@@ -1,13 +1,26 @@
 import { Link } from "@tanstack/react-router";
+import { useParams } from "@tanstack/react-router";
 import "./Topbar.css";
 
-export default function Topbar() {
+export function Topbar() {
+  const { projectId } = useParams({ strict: false });
+
+  if (!projectId) {
+    return (
+      <header className="app-layout__topbar topbar">
+        <nav className="topbar__nav">
+          <ul className="topbar__list">
+          </ul>
+        </nav>
+      </header>
+    );
+  }
   return (
-    <header className="topbar">
+    <header className="app-layout__topbar topbar">
       <nav className="topbar__nav">
         <ul className="topbar__list">
           <li className="topbar__item">
-            <Link to="/projects/$projectId/$backlog" className="topbar__link">
+            <Link to="/projects/$projectId/backlog" params={{projectId}} className="topbar__link">
               Backlog
             </Link>
           </li>

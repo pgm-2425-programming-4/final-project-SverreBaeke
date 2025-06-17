a6aab008197a164d6fdb193dd079c5709d94be7c
SverreBaeke <sverre.baeke@hotmail.com>
2025-06-17 14:24:28 +0200

    refactor: simplify AppLayout component structure and imports

--- client/src/components/AppLayout/AppLayout.jsx
+++ client/src/components/AppLayout/AppLayout.jsx
@@ -1,17 +1,13 @@
-import Sidebar from "../Sidebar/Sidebar.jsx";
-import Topbar from "../Topbar/Topbar.jsx";
+import { Sidebar } from "../Sidebar/Sidebar.jsx";
+import { Topbar } from "../Topbar/Topbar.jsx";
 import { Outlet } from "@tanstack/react-router";
 import "./AppLayout.css";
 
-export default function AppLayout() {
+export function AppLayout() {
   return (
     <div className="app-layout">
-      <aside className="app-layout__sidebar">
-        <Sidebar />
-      </aside>
-      <header className="app-layout__topbar">
-        <Topbar />
-      </header>
+      <Sidebar />
+      <Topbar />
       <main className="app-layout__content">
         <Outlet />
       </main>

8b958e021844bd550109e938ba02bc4a03df67f7
SverreBaeke <sverre.baeke@hotmail.com>
2025-06-18 11:07:56 +0200

    feat: integrate AddTaskModal into AppLayout for task management

--- client/src/components/AppLayout/AppLayout.jsx
+++ client/src/components/AppLayout/AppLayout.jsx
@@ -2,15 +2,37 @@ import { Sidebar } from "../Sidebar/Sidebar.jsx";
 import { Topbar } from "../Topbar/Topbar.jsx";
 import { Outlet } from "@tanstack/react-router";
 import "./AppLayout.css";
+import { AddTaskModal } from "../AddTaskModal/AddTaskModal.jsx";
+import { useState } from "react";
 
 export function AppLayout() {
+  const [isAddTaskModalOpen, setIsAddTaskModalOpen] = useState(false);
+
+  function handleAddTask() {
+    setIsAddTaskModalOpen(true);
+  }
+
+  function handleCloseAddTaskModal() {
+    setIsAddTaskModalOpen(false);
+  }
+
+  function handleTaskCreated(newTask) {
+    setIsAddTaskModalOpen(false);
+  }
   return (
     <div className="app-layout">
       <Sidebar />
-      <Topbar />
+      <Topbar handleAddTask={handleAddTask} />
       <main className="app-layout__content">
         <Outlet />
       </main>
+      <AddTaskModal
+        isOpen={isAddTaskModalOpen}
+        onClose={handleCloseAddTaskModal}
+        onTaskCreated={handleTaskCreated}
+        // statuses={}
+        // projectId={}
+      />
     </div>
   );
 }

c2d00bae9e1a469f587e49b2d5174d7e248eef60
SverreBaeke <sverre.baeke@hotmail.com>
2025-06-18 10:08:22 +0200

    feat: Added add task and board button to Topbar component

--- client/src/components/Topbar/Topbar.jsx
+++ client/src/components/Topbar/Topbar.jsx
@@ -2,15 +2,14 @@ import { Link } from "@tanstack/react-router";
 import { useParams } from "@tanstack/react-router";
 import "./Topbar.css";
 
-export function Topbar() {
+export function Topbar({handleAddTask}) {
   const { projectId } = useParams({ strict: false });
 
   if (!projectId) {
     return (
       <header className="app-layout__topbar topbar">
         <nav className="topbar__nav">
-          <ul className="topbar__list">
-          </ul>
+          <ul className="topbar__list"></ul>
         </nav>
       </header>
     );
@@ -24,7 +23,26 @@ export function Topbar() {
               Backlog
             </Link>
           </li>
+          <li className="topbar__item">
+            <Link
+              to="/projects/$projectId"
+              params={{ projectId }}
+              className="topbar__link"
+            >
+              Board
+            </Link>
+          </li>
         </ul>
+
+        <div className="topbar__actions">
+          <button
+            className="topbar__add-task-btn"
+            onClick={handleAddTask}
+            type="button"
+          >
+            + Add Task
+          </button>
+        </div>
       </nav>
     </header>
   );

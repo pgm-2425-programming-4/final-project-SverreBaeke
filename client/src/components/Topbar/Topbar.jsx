f9a8791fa3e2f9b7ef9d10c09fecc71357c467eb
SverreBaeke <sverre.baeke@hotmail.com>
2025-06-18 16:11:33 +0200

    refactor: update Topbar component structure and styles, integrate Add Task button

--- client/src/components/Topbar/Topbar.jsx
+++ client/src/components/Topbar/Topbar.jsx
@@ -1,39 +1,49 @@
 import { Link } from "@tanstack/react-router";
-import { useParams } from "@tanstack/react-router";
 import "./Topbar.css";
 
-export function Topbar() {
-  const { projectId } = useParams({ strict: false });
-
-  if (!projectId) {
-    return (
-      <header className="app-layout__topbar topbar">
-        <nav className="topbar__nav">
-          <ul className="topbar__list"></ul>
-        </nav>
-      </header>
-    );
-  }
+export function Topbar({ projectName, projectId, onAddTask }) {
   return (
-    <header className="app-layout__topbar topbar">
-      <nav className="topbar__nav">
-        <ul className="topbar__list">
-          <li className="topbar__item">
-            <Link to="/projects/$projectId/backlog" className="topbar__link">
-              Backlog
-            </Link>
-          </li>
-          <li className="topbar__item">
-            <Link
-              to="/projects/$projectId"
-              params={{ projectId }}
-              className="topbar__link"
+    <header className="topbar">
+      <div className="topbar__content">
+        <div className="topbar__title-section">
+          <h1 className="topbar__project-name">{projectName}</h1>
+        </div>
+
+        <nav className="topbar__nav">
+          <ul className="topbar__list">
+            <li className="topbar__item">
+              <Link
+                to="/projects/$projectId/backlog"
+                params={{ projectId }}
+                className="topbar__link"
+              >
+                Backlog
+              </Link>
+            </li>
+            <li className="topbar__item">
+              <Link
+                to="/projects/$projectId"
+                params={{ projectId }}
+                className="topbar__link"
+              >
+                Board
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        {onAddTask ? (
+          <div className="topbar__actions">
+            <button
+              className="topbar__add-task-btn"
+              onClick={onAddTask}
+              type="button"
             >
-              Board
-            </Link>
-          </li>
-        </ul>
-      </nav>
+              + Add Task
+            </button>
+          </div>
+        ) : null}
+      </div>
     </header>
   );
 }
--- client/src/routes/projects/$projectId/backlog/index.jsx
+++ client/src/routes/projects/$projectId/backlog/index.jsx
@@ -1,11 +1,24 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { PaginatedBackLog } from "../../../../components/PaginatedBacklog/paginated-Backlog";
+import { fetchProjectsById } from "../../../../data/fetchProjects";
+import { Topbar } from "../../../../components/Topbar/Topbar";
 
 export const Route = createFileRoute("/projects/$projectId/backlog/")({
+  loader: async ({ params }) => {
+    const project = await fetchProjectsById(params.projectId);
+    return { project };
+  },
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { project } = Route.useLoaderData();
   const { projectId } = Route.useParams();
-  return <PaginatedBackLog projectId={projectId} />;
+
+  return (
+    <>
+      <Topbar projectName={project.name} projectId={projectId} />
+      <PaginatedBackLog projectId={projectId} />;
+    </>
+  );
 }
